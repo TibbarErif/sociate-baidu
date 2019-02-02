0bac6d8e6ea327c80e406965f153ad5d3869dfaf
火烧兔子 <huoshaotuzi@icloud.com>
2019-02-02 21:16:48 +0800

    添加state参数

--- src/Firerabbit/Baidu/Oauth.php
+++ src/Firerabbit/Baidu/Oauth.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Firerabbit\Baidu;
 
 use GuzzleHttp\Client;
@@ -9,15 +10,16 @@ class Oauth
     const AUTHORITE_TOKEN_URL = 'https://openapi.baidu.com/oauth/2.0/token';
     const USER_INFO_URL = 'https://openapi.baidu.com/rest/2.0/passport/users/getInfo';
 
-    public static function getAuthoriteCodeUrl()
+    public static function getAuthoriteCodeUrl($state = '')
     {
         $params = [
             'client_id' => config('services.baidu.client_id'),
             'response_type' => 'code',
             'redirect_uri' => config('services.baidu.redirect'),
+            'state' => $state,
         ];
 
-        return self::AUTHORITE_CODE_URL . '?' . http_build_query($params);
+        return self::AUTHORITE_CODE_URL.'?'.http_build_query($params);
     }
 
     public static function getAccessToken($code)
